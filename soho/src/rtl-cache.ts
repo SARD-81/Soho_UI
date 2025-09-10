9c6c6da7f8ab72d20b89f756ae666a46c35f40ca
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2025-09-11 02:09:26 +0330

    Delete soho directory

--- soho/src/rtl-cache.ts
+++
@@ -1,7 +0,0 @@
-import createCache from '@emotion/cache';
-import stylisRTLPlugin from 'stylis-plugin-rtl';
-
-export const rtlCache = createCache({
-    key: 'mui-rtl',
-    stylisPlugins: [stylisRTLPlugin],
-});
