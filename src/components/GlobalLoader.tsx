b7e3970514a941f5fafc6dd7bb5a37fff304b09b
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2025-09-14 11:29:16 +0330

    feat: avoid loader on background fetches

---
+++ src/components/GlobalLoader.tsx
@@ -0,0 +1,16 @@
+import LoadingPage from './LoadingPage.tsx';
+import { useIsFetching, useIsMutating } from '@tanstack/react-query';
+
+export default function GlobalLoader() {
+  const isFetching = useIsFetching({
+    predicate: (query) =>
+      !query.meta?.skipGlobalLoader &&
+      query.state.fetchStatus === 'fetching' &&
+      query.state.data === undefined,
+  });
+  const isMutating = useIsMutating({
+    predicate: (mutation) => !mutation.meta?.skipGlobalLoader,
+  });
+
+  return isFetching + isMutating > 0 ? <LoadingPage /> : null;
+}
