2b5795b35ddeec0d999da435ea07fc757fe09600
AmirRezaDavarzani <am.davarzani@storex.local>
2025-10-18 08:24:26 +0330

    14040726 08:24  project scrum

--- src/components/share/SharesTable.tsx
+++ src/components/share/SharesTable.tsx
@@ -168,7 +168,7 @@ const SharesTable = ({
       isLoading={isLoading}
       error={error}
       onRowClick={handleRowClick}
-      bodyRowSx={(share) => ({
+      bodyRowSx={(share: SambaShareEntry) => ({
         ...resolveRowSx(share),
         transition: 'background-color 0.2s ease',
       })}
