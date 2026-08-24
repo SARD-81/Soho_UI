948675b98eac2b9292303cd517b9af060ccab43a
S.Amir Reza Davarzani <116599983+SARD-81@users.noreply.github.com>
2026-08-24 10:16:43 +0330

    refactor: remove dead navigation comments

--- src/constants/navigationDrawer.ts
+++ src/constants/navigationDrawer.ts
@@ -1,9 +1,11 @@
 import { createElement } from 'react';
-// import { BiHistory } from 'react-icons/bi';
 import { BsDeviceHdd, BsFolderSymlink, BsHddNetwork } from 'react-icons/bs';
-// import { FiUsers } from 'react-icons/fi';
 import { GrServices, GrStorage } from 'react-icons/gr';
-import { MdAccountTree, MdOutlinePublic, MdSpaceDashboard } from 'react-icons/md';
+import {
+  MdAccountTree,
+  MdOutlinePublic,
+  MdSpaceDashboard,
+} from 'react-icons/md';
 import { RiSettings3Fill, RiShareForwardLine } from 'react-icons/ri';
 import { TbActivityHeartbeat, TbServerCog } from 'react-icons/tb';
 import type { NavigationItem } from '../@types/navigationDrawer';
@@ -26,11 +28,6 @@ export const navItems: NavigationItem[] = [
     icon: createElement(GrStorage),
     path: '/Integrated-space',
   },
-  // {
-  //   text: 'فضای بلاکی',
-  //   icon: createElement(MdStorage),
-  //   path: '/block-space',
-  // },
   {
     text: 'فضای فایلی',
     icon: createElement(MdAccountTree),
@@ -39,7 +36,6 @@ export const navItems: NavigationItem[] = [
   {
     text: 'اشتراک گذاری',
     icon: createElement(RiShareForwardLine),
-    // path: '/share',
     children: [
       { text: 'SMB', icon: createElement(BsFolderSymlink), path: '/share' },
       { text: 'NFS', icon: createElement(BsHddNetwork), path: '/share-nfs' },
@@ -53,7 +49,6 @@ export const navItems: NavigationItem[] = [
   {
     text: 'سرویس ها',
     icon: createElement(GrServices),
-    // path: '/',
     children: [
       {
         text: 'سرویس ها',
@@ -67,14 +62,9 @@ export const navItems: NavigationItem[] = [
       },
     ],
   },
-
   {
     text: 'تنظیمات',
     icon: createElement(RiSettings3Fill),
     path: '/settings',
-    // children: [
-    //   { text: 'کاربران', icon: createElement(FiUsers), path: '/users' },
-    // ],
   },
-  // { text: 'تاریخچه', icon: createElement(BiHistory), path: '/history' },
 ];
